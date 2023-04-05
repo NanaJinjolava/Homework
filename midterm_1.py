4e80dfd00e361f902736c4b24cc444eaf005a17a
NanaJinjolava <n.jinjolava3@gmail.com>
2023-04-05 19:48:24 +0400

    add midterm 1

---
+++ midterm_1.py
@@ -0,0 +1,39 @@
+# exercise 1
+class Person:
+    def __init__(self, name, gender, age, income):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.income = income
+
+    def __str__(self):
+        return f"name:{self.name} \ngender: {self.gender}" \
+               f" \nage: {self.age} \nincome: {self.income}"
+
+    def monthlytax(self):
+        tax = self.income * 0.2 + self.income * 0.01
+        return f"tax wage is {tax}"
+
+    def retirementmortgage(self):
+        if self.gender == "male":
+            ret_money = (65 - self.age) * 12 * (self.income * 0.01)
+            return f"retirement mortgage amounts to {ret_money}"
+
+        if self.gender == "female":
+            ret_money = (60 - self.age) * 12 * (self.income * 0.01)
+            return f"retirement mortgage amounts to {ret_money}"
+
+    def retirement_age(self):
+        if self.gender == "male":
+            return 65 - self.age
+
+        elif self.gender == "female":
+            return 60 - self.age
+
+
+per_1 = Person("Nana", "female", 18, 900)
+per_2 = Person("Giorgi", "male", 21, 1000)
+print(per_1.__str__(), '\n', per_1.monthlytax(),
+      '\n', per_1.retirementmortgage(), '\n', per_1.retirement_age())
+print(per_2.__str__(), '\n', per_2.monthlytax(),
+      '\n', per_2.retirementmortgage(), '\n', per_2.retirement_age())
